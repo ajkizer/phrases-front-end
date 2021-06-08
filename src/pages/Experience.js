281eb47561d3eda0e29eb2fca9dce3db3b39e14a
Anthony Kizer <anthony.kizer88@gmail.com>
2021-06-08 14:45:18 -0700

    added functionality for adding a phrase to an experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,18 +1,17 @@
-import React, {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom';
-import {devMain} from '../utils/apiURLs';
+import React, { useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom';
+import { devMain } from '../utils/apiURLs';
 import axios from 'axios';
-import { Card, Col } from 'react-bootstrap';
+import { Card, Col, Modal, Button, Form, Row } from 'react-bootstrap';
 
 const Experience = () => {
-    const param = useParams();
- 
     const [phrases, setPhrases] = useState([]);
-
     const [loading, toggleLoading] = useState(true);
 
+    const param = useParams();
+
     const getPhrases = async () => {
-       const res =  await axios.get(`${devMain}/experiences/${param.id}/phrases`)
+        const res = await axios.get(`${devMain}/experiences/${param.id}/phrases`)
         setPhrases(res.data.data);
         toggleLoading(false);
     }
@@ -20,17 +19,74 @@ const Experience = () => {
 
     useEffect(() => {
         getPhrases();
-    },[])
+    }, [])
 
-    const Phrases = () => {
-        return (<div className="phrases">{phrases.map(phrase => <Col md={{span: 6, offset: 3}}><Card><Card.Body><Card.Text>{phrase.meaning}</Card.Text><Card.Text><em>"{phrase.phrase}"</em></Card.Text></Card.Body></Card></Col>)}</div>)
+
+
+
+    const PhraseCard = ({ phrase }) => {
+        return (<Col md={{ span: 6, offset: 3 }}><Link to={`/dashboard/experiences/${param.id}/phrases/${phrase._id}`}><Card className="phrases__card"><Card.Body><Card.Text>{phrase.meaning}</Card.Text><Card.Text><em>"{phrase.phrase}"</em></Card.Text></Card.Body></Card></Link></Col>)
     }
 
-    console.log(phrases);
+    const Phrases = () => {
+        return (<div className="phrases">{phrases.map(phrase => <PhraseCard phrase={phrase} />)}</div>)
+    }
+
+    const AddPhrase = () => {
+        const [show, setShow] = useState(false);
+        const [formData, setFormData] = useState({
+            phrase: "",
+            meaning: ""
+        })
+
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
+
+
+        const { phrase, meaning } = formData;
+
+
+        const addPhrase = async () => {
+            const URL = `${devMain}/experiences/${param.id}/phrases`
+            try {
+                const res = await axios.post(URL, formData)
+                console.log(res);
+                handleClose();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        const changeHandler = e => {
+            setFormData({ ...formData, [e.target.name]: e.target.value })
+        }
+
+
+        return (<div className="phrases__add-phrase"> <Button onClick={handleShow}>
+            Add Phrase
+        </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Phrase</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={addPhrase}>
+                        <Form.Group><Form.Label>Phrase</Form.Label><Form.Control onChange={changeHandler} value={phrase} name="phrase" type="text" placeholder="" /></Form.Group>
+                        <Form.Group><Form.Label>Meaning</Form.Label><Form.Control value={meaning} onChange={changeHandler} name="meaning" type="text" placeholder="" /></Form.Group>
+                        <Button type="submit">Submit</Button>
+                    </Form>
+
+                </Modal.Body>
+
+            </Modal></div>)
+    }
+
     return (
         <div>
+            <AddPhrase />
             Experience {param.id}
-            <div>{loading ? "loading..." : <Phrases/>}</div>
+            <div>{loading ? "loading..." : <Phrases />}</div>
         </div>
     )
 }
