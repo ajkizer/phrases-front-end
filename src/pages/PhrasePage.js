4525eb7fccc77853452e40313a1a0e2c348ef1b6
Anthony Kizer <anthony.kizer88@gmail.com>
2021-06-09 15:07:20 -0700

    added functionality for adding variation

--- src/pages/PhrasePage.js
+++ src/pages/PhrasePage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { devMain } from '../utils/apiURLs'
-import { Card, Row, Col, Button } from 'react-bootstrap'
+import { Card, Row, Col, Button, Form, Modal } from 'react-bootstrap'
 
 const PhrasePage = () => {
     const [phrase, setPhrase] = useState({})
@@ -14,6 +14,11 @@ const PhrasePage = () => {
         const res = await axios.get(`${devMain}/phrases/${id}`)
         setPhrase(res.data.data);
         toggleLoading(false);
+    }
+
+
+    const updatePhrase = (item) => {
+        setPhrase({...phrase, variations: [...phrase.variations, item]})
     }
 
     useEffect(() => {
@@ -52,6 +57,55 @@ const PhrasePage = () => {
         )
     }
 
+    const AddVariation = ({ updatePhrase }) => {
+        const [show, setShow] = useState(false);
+        const [formData, setFormData] = useState({
+            phrase: "",
+            meaning: ""
+        })
+
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
+
+
+        const { phrase, meaning } = formData;
+
+
+        const addVariation = async (e) => {
+            e.preventDefault();
+            const URL = `${devMain}/phrases/${id}/variations`
+            try {
+                const res = await axios.put(URL, formData);
+                updatePhrase(res.data.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        const changeHandler = e => {
+            setFormData({ ...formData, [e.target.name]: e.target.value })
+        }
+
+
+        return (<div className="phrases__add-variation"> <Button onClick={handleShow}>
+            Add Variation
+        </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Variation</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={addVariation}>
+                        <Form.Group><Form.Label>Phrase</Form.Label><Form.Control onChange={changeHandler} value={phrase} name="phrase" type="text" placeholder="" /></Form.Group>
+                        <Form.Group><Form.Label>Meaning</Form.Label><Form.Control value={meaning} onChange={changeHandler} name="meaning" type="text" placeholder="" /></Form.Group>
+                        <Button type="submit">Submit</Button>
+                    </Form>
+
+                </Modal.Body>
+
+            </Modal></div>)
+    }
     return (
 
         loading ? <>"loading"</> : <div>
@@ -61,6 +115,7 @@ const PhrasePage = () => {
                 <Card>
                     <h2>Phrase: {phrase.phrase}</h2>
                     <p>{phrase.meaning}</p>
+                    <AddVariation updatePhrase={updatePhrase}/>
                 </Card>
             </Col>
             <Row>
