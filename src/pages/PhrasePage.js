73cc1051adecb208ab31c40e34f4956611883334
Anthony Kizer <anthony.kizer88@gmail.com>
2021-06-10 19:29:35 -0700

    editing a child now works with new endpoints

--- src/pages/PhrasePage.js
+++ src/pages/PhrasePage.js
@@ -5,7 +5,8 @@ import { devMain } from '../utils/apiURLs'
 import { Card, Row, Col, Button, Form, Modal } from 'react-bootstrap'
 
 const PhrasePage = () => {
-    const [phrase, setPhrase] = useState({})
+    const [currentPhrase, setCurrentPhrase] = useState({})
+    const [children, setChildren] = useState([])
     const [loading, toggleLoading] = useState(true);
     const [mode, setMode] = useState("viewAll")
     const { id } = useParams();
@@ -13,13 +14,14 @@ const PhrasePage = () => {
 
     const getPhrase = async () => {
         const res = await axios.get(`${devMain}/phrases/${id}`)
-        setPhrase(res.data.data);
+        setCurrentPhrase(res.data.data.phrase);
+        setChildren(res.data.data.children);
         toggleLoading(false);
     }
 
 
     const updatePhrase = (item) => {
-        setPhrase({ ...phrase, variations: [...phrase.variations, item] })
+        setCurrentPhrase({ ...currentPhrase, variations: [...currentPhrase.variations, item] })
     }
 
     useEffect(() => {
@@ -30,21 +32,73 @@ const PhrasePage = () => {
     const viewAll = () => setMode("viewAll")
 
 
-    const Variation = ({ variation }) => {
-        return (
-            <Col className="mx-auto" md={{ span: 4 }}>
-                <Card>
-                    <p>Phrase: {variation.phrase}</p>
-                    <p>Meaning: {variation.meaning}</p>
-                </Card>
-            </Col>
-        )
-    }
-
     console.log(mode)
 
 
-    const FlashCard = ({ variation }) => {
+    const EditPhrase = ({ child }) => {
+        const [show, setShow] = useState(false);
+        const [formData, setFormData] = useState({
+            phrase: child.phrase,
+            meaning: child.meaning
+        })
+
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
+
+
+        const { phrase, meaning } = formData;
+
+
+        const editChild = async (e) => {
+            e.preventDefault();
+            const URL = `${devMain}/phrases/${child._id}`
+
+            try {
+                const updateIndex = children.map(item => item._id).indexOf(child._id);
+                setChildren([...children.slice(0, updateIndex), {...children[updateIndex], ...formData}, ...children.slice(updateIndex+1)])
+                const res = await axios.put(URL, formData)
+                
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        const changeHandler = e => {
+            setFormData({ ...formData, [e.target.name]: e.target.value })
+        }
+
+
+
+
+
+        return (<div className="phrases__add-variation"> <Button variant="light" onClick={handleShow}>
+            <i class="fad fa-pencil-alt"></i>
+        </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Variation</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={editChild}>
+                        <Form.Group><Form.Label>Phrase</Form.Label><Form.Control onChange={changeHandler} value={phrase} name="phrase" type="text" placeholder="" /></Form.Group>
+                        <Form.Group><Form.Label>Meaning</Form.Label><Form.Control value={meaning} onChange={changeHandler} name="meaning" type="text" placeholder="" /></Form.Group>
+                        <Button type="submit">Submit</Button>
+                    </Form>
+
+                </Modal.Body>
+
+            </Modal></div>)
+    }
+
+
+
+
+
+
+
+
+    const FlashCard = ({ child, showNext }) => {
         const [showAnswer, setShowAnswer] = useState(false)
 
         const handleHide = () => setShowAnswer(false)
@@ -54,14 +108,35 @@ const PhrasePage = () => {
         return (
             <Col className="mx-auto" md={{ span: 7 }}>
                 <Card>
-                    <p>Phrase: {variation.phrase}</p>
-                    {showAnswer ? <><p>Meaning: {variation.meaning}</p><div><Button onClick={handleHide}>Hide</Button></div></> : <div><p><em>Answer Hidden</em></p><Button onClick={handleShow}>Show Answer</Button> </ div >}
+                    <p>Phrase: {child.phrase}</p>
+                    {showAnswer ? <><p>Meaning: {child.meaning}</p><div><Button onClick={handleHide}>Hide</Button><Button onClick={showNext}>Next</Button></div></> : <div><p><em>Answer Hidden</em></p><Button onClick={handleShow}>Show Answer</Button> </ div >}
                 </Card>
             </Col>
         )
     }
 
-    const AddVariation = ({ updatePhrase }) => {
+
+
+
+    const Variation = ({ child }) => {
+
+        return (
+            <Col className="mx-auto" md={{ span: 4 }}>
+                <Card>
+                    <Card.Body>
+                        <EditPhrase child={child} />
+                        <p>Phrase: {child.phrase}</p>
+                        <p>Meaning: {child.meaning}</p>
+                    </Card.Body>
+                </Card>
+            </Col>
+        )
+    }
+
+
+
+
+    const AddChild = ({ updatePhrase }) => {
         const [show, setShow] = useState(false);
         const [formData, setFormData] = useState({
             phrase: "",
@@ -77,7 +152,7 @@ const PhrasePage = () => {
 
         const addVariation = async (e) => {
             e.preventDefault();
-            const URL = `${devMain}/phrases/${id}/variations`
+            const URL = `${devMain}/phrases/${id}`
             try {
                 const res = await axios.put(URL, formData);
                 updatePhrase(res.data.data);
@@ -94,13 +169,13 @@ const PhrasePage = () => {
 
 
 
-        return (<div className="phrases__add-variation"> <Button onClick={handleShow}>
-            Add Variation
+        return (<div className="phrases__add-variation"> <Button variant="light" onClick={handleShow}>
+            <i class="fas fa-comment-alt-lines"></i>
         </Button>
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Add Variation</Modal.Title>
+                    <Modal.Title>Add Phrase Variation</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form onSubmit={addVariation}>
@@ -117,7 +192,10 @@ const PhrasePage = () => {
 
 
 
-    const Study = ({ variations }) => {
+    const Study = ({ children }) => {
+        const [cards, setCards] = useState()
+
+
 
 
         const shuffleArray = array => {
@@ -130,17 +208,39 @@ const PhrasePage = () => {
         }
 
 
-        const shuffled = shuffleArray(variations);
 
-        return (<div>{shuffled.length && shuffled.map(item => <FlashCard variation={item} />)}</div>)
+        useEffect(() => {
+            const deck = shuffleArray(children);
+            setCards(deck);
+        }, [])
+
+
+
+        const showNext = () => {
+            let deck = cards;
+
+            const currentCard = deck[0]
+
+            deck.push(currentCard);
+
+            deck.shift();
+
+            console.log(deck);
+
+
+            setCards([...deck])
+        }
+
+        return (<div>{cards && <FlashCard variation={cards[0]} showNext={showNext} />}</div>)
 
     }
 
 
 
 
-    const TakeNotes = () => {
-        return (<div></div>)
+    const TakeNotes = ({ children }) => {
+
+        return (<div>{children.map(item => <Variation key={item._id} child={item} />)}</div>)
     }
 
     return (
@@ -150,19 +250,22 @@ const PhrasePage = () => {
 
             <Col md={{ span: 7 }} className="mx-auto mb-2">
                 <Card>
-                    <h2>Phrase: {phrase.phrase}</h2>
-                    <p>{phrase.meaning}</p>
-                    <Row>
-                        <Col>
-                            <AddVariation updatePhrase={updatePhrase} />
-                            <div><Button variant="light" onClick={startStudySession}><i class="fas fa-books"></i></Button></div>
-                            <div><Button variant="light" onClick={viewAll}><i class="fad fa-pencil-alt"></i></Button></div>
-                        </Col>
-                    </Row>
+                    <h2>Phrase: {currentPhrase.phrase}</h2>
+                    <p>{currentPhrase.meaning}</p>
+
+
+                    <AddChild updatePhrase={updatePhrase} />
+
+
+                    <div><Button variant="light" onClick={startStudySession}><i class="fas fa-books"></i></Button></div>
+
+
+                    <div><Button variant="light" onClick={viewAll}><i class="fad fa-pencil-alt"></i></Button></div>
+
                 </Card>
             </Col>
             <Row>
-                <Study variations={phrase.variations} />
+                {mode === "flashCards" ? <Study children={children} /> : <TakeNotes children={children} />}
             </Row>
         </div >)
 }
