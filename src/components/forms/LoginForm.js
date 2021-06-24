ca1b126d670001ee5e4a22258ddad9d22b5c5bb3
Anthony Kizer <anthony.kizer88@gmail.com>
2021-06-24 16:03:48 -0700

    login form moved to its own component

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import { Form, Col, Button } from 'react-bootstrap'
+import axios from 'axios';
+import setAuthToken from '../../utils/setAuthToken';
+import {auth} from '../../utils/apiURLs'
+
+const LoginForm = () => {
+
+    const [formData, setFormData] = useState({
+        username: "",
+        password: ""
+    });
+
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    
+    const login = async () => {
+        let url = `${auth}/login`
+
+
+        try {
+            let res = await axios.post(url, formData);
+
+            localStorage.setItem("token", res.data.token);
+
+            url = `${auth}/me`
+
+
+            setAuthToken(res.data.token);
+
+            window.location.href = "/dashboard"
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const {username, password} = formData;
+
+    const onSubmit = e => {
+        e.preventDefault();
+        login();
+    }
+
+    return (<><h3>Login to Profile</h3>
+        <Form onSubmit={onSubmit}>
+
+            <Form.Group controlId="login.name">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" value={username} name="username" onChange={onChange} />
+            </Form.Group>
+            <Form.Group controlId="login.password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" value={password} name="password" onChange={onChange} />
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+        </Form></>)
+}
+
+export default LoginForm
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,101 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Col, Button } from 'react-bootstrap'
-import axios from 'axios';
-import setAuthToken from '../utils/setAuthToken';
+import LoginForm from "../components/forms/LoginForm"
 
-import { auth } from '../utils/apiURLs';
+
 
 const Login = () => {
 
     const checkIfLoggedIn = () => {
-        let user = JSON.parse(localStorage.getItem("phrasesCurrentUser"));
+        const token = localStorage.getItem("token");
 
-        if (!user) return
+        if(token) {
+            window.location.href="/dashboard"
+        }
     }
-
     useEffect(() => {
         checkIfLoggedIn()
     }, [])
 
-
-
-    const login = async () => {
-        let url = `${auth}/login`
-
-
-        try {
-            let res = await axios.post(url, formData);
-
-            localStorage.setItem("token", res.data.token);
-
-            url = `${auth}/me`
-
-
-            setAuthToken(res.data.token);
-
-            window.location.href = "/dashboard"
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    const [formData, setFormData] = useState({
-        username: "",
-        password: ""
-    });
-
-    const { username, password } = formData;
-
-    const onChange = e => {
-        setFormData({ ...formData, [e.target.name]: e.target.value })
-    }
-
-
-
-    const LoginForm = () => {
-        const onSubmit = e => {
-            e.preventDefault();
-            login();
-        }
-
-        return (<><h3>Login to Profile</h3>
-            <Form onSubmit={onSubmit}>
-
-                <Form.Group controlId="login.name">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" value={username} name="username" onChange={onChange} />
-                </Form.Group>
-                <Form.Group controlId="login.password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" value={password} name="password" onChange={onChange} />
-                </Form.Group>
-                <Button type="submit">Submit</Button>
-            </Form></>)
-    }
-
-
-    const SignUpForm = () => {
-        const onSubmit = e => {
-            e.preventDefault();
-        }
-
-        return (<><h3>Login to Profile</h3>
-            <Form onSubmit={onSubmit}>
-                <Form.Group controlId="signup.name">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" value={username} name="username" onChange={onChange} />
-                </Form.Group>
-                <Form.Group controlId="signup.name">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" value={username} name="username" onChange={onChange} />
-                </Form.Group>
-                <Form.Group controlId="signup.password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" value={password} name="password" onChange={onChange} />
-                </Form.Group>
-                <Button type="submit">Submit</Button>
-            </Form></>)
-    }
+  
 
     return (
         <Col xs={{ span: 12 }} md={{ span: 6, offset: 3 }}>
