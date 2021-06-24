d5cd18ba6386d3ef6a1fa7408d1e9673b284c02a
Anthony Kizer <anthony.kizer88@gmail.com>
2021-06-24 16:18:48 -0700

    created sign up form

--- src/components/forms/SignUpForm.js
+++ src/components/forms/SignUpForm.js
@@ -1,11 +1,72 @@
-import React from 'react'
+
+
+import React, {useState} from 'react'
+import { Form, Col, Button } from 'react-bootstrap'
+import axios from 'axios';
+import setAuthToken from '../../utils/setAuthToken';
+import {auth} from '../../utils/apiURLs'
 
 const SignUpForm = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    
+    const register = async () => {
+        let url = `${auth}/register`
+
+
+        let formDataCopy = formData;
+
+        formDataCopy.role = "customer"
+
+        try {
+            let res = await axios.post(url, formDataCopy);
+
+            localStorage.setItem("token", res.data.token);
+
+            setAuthToken(res.data.token);
+
+            window.location.href = "/dashboard"
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const {username, password, email} = formData;
+
+    const onSubmit = e => {
+        e.preventDefault();
+        register();
+    }
+
+    return (<><h3>Login to Profile</h3>
+        <Form onSubmit={onSubmit}>
+
+            <Form.Group controlId="login.name">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" value={username} name="username" onChange={onChange} />
+            </Form.Group>
+            <Form.Group controlId="login.name">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="text" value={email} name="email" onChange={onChange} />
+            </Form.Group>
+            <Form.Group controlId="login.password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" value={password} name="password" onChange={onChange} />
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+        </Form></>)
 }
+
+
+
 
 export default SignUpForm
